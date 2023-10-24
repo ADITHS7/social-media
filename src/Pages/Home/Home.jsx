076b8c3689152e46b254a131f05843fdfcbde7d8
ADITHS7 <adithsn777@gmail.com>
2023-10-24 23:35:33 +0530

    social media app just started header almost completed

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.css'
+import Topbar from '../../Components/Topbar/Topbar'
+const Home = () => {
+  return (
+    <div><Topbar/></div>
+  )
+}
+
+export default Home
